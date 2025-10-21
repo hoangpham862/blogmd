17226d8eda36509b2164c5dcef336f1b747158b4
hoang.pham <hoang.pham@fssc.com.vn>
2025-10-21 16:36:15 +0700

    update: thay đổi cấu trúc dự án

---
+++ TUDUYLAPTRINH/main/src/main/java/com/hquiz/MakeHighestAge.java
@@ -0,0 +1,12 @@
+package com.hquiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MakeHighestAge {
+    List<Integer> ages = new ArrayList<Integer>();
+
+    public void setAges(List<Integer> ages) {
+        this.ages = ages;
+    }
+}
